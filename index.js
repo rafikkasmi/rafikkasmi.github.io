9eadcfad9cab08ecbd1b548c932d58a871279fca
rafikhdey <rafikhdey@gmail.com>
2018-08-26 13:03:30 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 AOS.init();
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-        
 
 let y = $('.About').position().top;
 
